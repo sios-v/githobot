d76f2b12cc5f016d66c66f442d6037ffc9a5a562
Gadjimurad <50250997+sios-v@users.noreply.github.com>
2023-05-19 09:13:31 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let function{
+	class_name.prototype.method_name = function(first_argument) {
+		// body...
+	class_name: first_argument class_name
+	};
+	#!/usr/bin/env 
+	env
+	for (var i = 0; i < Things.length; i++) {
+		Things[i]
+	}
+}
